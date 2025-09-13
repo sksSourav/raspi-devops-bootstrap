1cac85d401c58c97b29d9b19e1383a4bb2e8ca25
Sourav <sourav.insa@gmail.com>
2025-09-13 15:48:44 +0200

    Update instal_az_agent.sh

--- instal_az_agent.sh
+++ instal_az_agent.sh
@@ -8,19 +8,19 @@ sudo apt-get autoclean
 sudo apt-get clean
 sudo reboot
 ### Install git ###
-sudo apt install git
+sudo apt install git -y
 ### Setup azure agent ###
 sudo mkdir .devops_azure_agent
 cd .devops_azure_agent
-curl -L -o agent.tar.gz https://vstsagentpackage.azureedge.net/agent/3.241.1/vsts-agent-linux-x64-3.241.1.tar.gz
-sudo tar zxvf ~/Downloads/vsts-agent-osx-x64-4.261.0.tar.gz
+sudo curl -L -o agent.tar.gz <url_from_website>
+sudo tar zxvf agent.tar.gz
 ### Execute config script to setup agent. Provide requested input like azure pool, key, etc. ###
-sudo ./config.sh
+./config.sh
 ### Install & Start the agent ###
 sudo ./svc.sh install
 sudo ./svc.sh start
 ### Add agent to system startup ###
-sudo systemctl enable vsts.agent.souravksahu.skspi5in.skspi5in.service
+sudo systemctl enable vsts.agent.souravksahu.<pool>.<agent>.service
 
 ###### check status of service ######
 # sudo systemctl status vsts.agent.*.service
